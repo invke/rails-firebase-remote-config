970c946baae305e445c253222b623d961a2f1a1a
Montgomery Anderson <montgomery.c.anderson@gmail.com>
2022-03-11 10:46:23 +1300

    feat: added configuration setups and adapter config

--- lib/remote_config.rb
+++ lib/remote_config.rb
@@ -2,5 +2,32 @@
 
 require "remote_config/version"
 require "remote_config/engine"
+require "remote_config/adapters/base"
+require "remote_config/adapters/ruby_config_adapter"
 
-module RemoteConfig; end
+module RemoteConfig
+  Configuration = Struct.new(
+    # A string to constantize to get the adapter
+    :adapter,
+
+    # The options hash to be passed to the adapter at initialization
+    :adapter_options,
+  )
+
+  class << self
+    def configure
+      yield configuration
+    end
+
+    def configuration
+      @configuration ||= Configuration.new(
+        adapter:         Adapters::RubyConfigAdapter,
+        adapter_options: {},
+      )
+    end
+
+    def adapter
+      @adapter ||= Configuration.adapter.constantize.new(Configuration.adapter_options)
+    end
+  end
+end
